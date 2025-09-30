38b009d64fd4e0136b474313d7aa6476ec13c351
RitikaBhadani11 <ritikabhadani98@gmail.com>
2025-10-01 00:07:57 +0530

    Add deployed frontend URL to CORS

--- server.js
+++ server.js
@@ -567,10 +567,13 @@ const io = socketIo(server, {
 
 // Middleware
 app.use(cors({
-  origin: "*",
+  origin: [
+    'https://collegeconnect-frontend.onrender.com', // Add this
+    'http://localhost:3000',
+    'http://localhost:5173'
+  ],
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  // ... rest of config
 }));
 
 app.use(morgan("dev"));
